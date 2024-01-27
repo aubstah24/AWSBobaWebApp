80978ab593c6cea5a5357db50880c1084b70cbb3
Aubrie Usui <127988637+aubstah24@users.noreply.github.com>
2024-01-26 15:36:58 -1000

    Created Navigation menu and backend routing for the top menu

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,27 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {AboutUsPage} from "./Pages/AboutUsPage";
+import {LandingPage} from "./Pages/LandingPage";
+import {MenuPage} from "./Pages/MenuPage";
+import {OnlineOrderingPage} from "./Pages/OnlineOrderingPage";
+import Navbar from "./Navbar";
+import {Route , Routes} from "react-router-dom";
 
 function App() {
+
   return (
-    <div className="App">
+      <>
+    <Navbar/>
+    <div className="container">
+      <Routes>
+        <Route path="/" element={<LandingPage/>}></Route>
+        <Route path="/menu" element={<MenuPage/>}></Route>
+        <Route path="/order" element={<OnlineOrderingPage/>}></Route>
+        <Route path="/about" element={<AboutUsPage/>}></Route>
+      </Routes>
+    </div>
+      </>
+    /*<div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,7 +36,7 @@ function App() {
           Learn React
         </a>
       </header>
-    </div>
+    </div>*/
   );
 }
 
---
+++ my-app/src/Navbar.js
@@ -0,0 +1,26 @@
+import {Link , useMatch, useResolvedPath} from "react-router-dom";
+
+export default function Navbar() {
+    const path = window.location.pathname
+    return (
+    <nav className="nav">
+        <Link to="/" className="site-title">Site Name</Link>
+        <ul>
+            <CustomLink to="/">HOME</CustomLink>
+            <CustomLink to="/menu">MENU</CustomLink>
+            <CustomLink to="/order">ONLINE ORDER</CustomLink>
+            <CustomLink to="/about">ABOUT US</CustomLink>
+        </ul>
+    </nav>
+    )
+}
+
+function CustomLink({to, children,...props}){
+    const resolvedPath = useResolvedPath(to)
+    const isActive = useMatch({ path: resolvedPath.pathname, end: true })
+    return (
+        <li className={isActive ? "active" : ""}>
+            <Link to={to} {...props}>{children}</Link>
+        </li>
+    )
+}
