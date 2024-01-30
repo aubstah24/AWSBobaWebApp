0e7760fa35f3d728cbf80e67e54d86705fb0117f
Aubrie Usui <127988637+aubstah24@users.noreply.github.com>
2024-01-30 12:27:53 -1000

    added in sweetness levels and color gradient.  Need to fix the right box height level

--- my-app/src/Pages/MenuPage.js
+++ my-app/src/Pages/MenuPage.js
@@ -14,22 +14,36 @@ export const MenuPage = () => {
     return (
         <Container className='menupage'>
             <Header className='pageheader' as='h1'>OUR MENU</Header>
-                <Card.Group>
-                    <Card fluid>
-                        <Image floated="left" src={filler} size='small' alt="Filler"/>
-                        <Card.Content>
-                            <Card.Header>BOBA DRINK EXAMPLE 1</Card.Header>
-                            <p>Here is a description of the boba like what flavors are in this and that it has syrups or extra sauces inside.</p>
-                        </Card.Content>
-                    </Card>
-                </Card.Group>
+
+            <div className='sweetnesslevel'>
+                <p className='sweetfull'>100% Sweet</p>
+
+                <p className='sweet3'>75% Sweet</p>
+
+                <p className='sweethalf'>50% Sweet</p>
+
+                <p className='sweetquarter'>25% Sweet</p>
+
+                <p className='unsweet'>0% Sweet</p>
+            </div>
+            <Card.Group>
+                <Card fluid>
+                    <Image floated="left" src={filler} size='small' alt="Filler"/>
+                    <Card.Content>
+                        <Card.Header>BOBA DRINK EXAMPLE 1</Card.Header>
+                        <p>Here is a description of the boba like what flavors are in this and that it has syrups or
+                            extra sauces inside.</p>
+                    </Card.Content>
+                </Card>
+            </Card.Group>
 
             <Card.Group>
                 <Card fluid>
                     <Image floated="left" src={coffee} size='small' alt="Filler"/>
                     <Card.Content>
                         <Card.Header>COFFEE DRINK EXAMPLE 2</Card.Header>
-                        <p>Here is a description of the boba like what flavors are in this and that it has syrups or extra sauces inside.</p>
+                        <p>Here is a description of the boba like what flavors are in this and that it has syrups or
+                            extra sauces inside.</p>
                     </Card.Content>
                 </Card>
             </Card.Group>
@@ -39,7 +53,8 @@ export const MenuPage = () => {
                     <Image floated="left" src={milk} size='small' alt="Filler"/>
                     <Card.Content>
                         <Card.Header>SUBSTITUTES FOR MILK</Card.Header>
-                        <p>Here is a description of the boba like what flavors are in this and that it has syrups or extra sauces inside.</p>
+                        <p>Here is a description of the boba like what flavors are in this and that it has syrups or
+                            extra sauces inside.</p>
                     </Card.Content>
                 </Card>
             </Card.Group>
