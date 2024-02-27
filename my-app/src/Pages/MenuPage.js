a7310913c2ad55db26d1fb1913344bd8a5b9a4d1
Aubrie Usui <127988637+aubstah24@users.noreply.github.com>
2024-02-26 14:58:36 -1000

    adding in a location page in navbar changed src to link to stripe payment portal

--- my-app/src/Pages/MenuPage.js
+++ my-app/src/Pages/MenuPage.js
@@ -107,38 +107,63 @@ export const MenuPage = () => {
                 </GridRow>
             </Grid>
 
-            <Card.Group>
-                <Card fluid>
-                    <Image floated="left" src={filler} size='small' alt="Filler"/>
-                    <Card.Content>
-                        <Card.Header>BOBA DRINK EXAMPLE 1</Card.Header>
-                        <p>Here is a description of the boba like what flavors are in this and that it has syrups or
-                            extra sauces inside.</p>
-                    </Card.Content>
-                </Card>
-            </Card.Group>
+            <div className="cards">
+            <Grid>
+                <GridRow columns={2}>
+                    <GridColumn>
+                        <Card.Group>
+                            <Card fluid>
+                                <Image floated="left" src={filler} alt="Filler"/>
+                                <Card.Content>
+                                    <Card.Header>BOBA DRINK EXAMPLE 1</Card.Header>
+                                    <p>Here is a description of the boba like what flavors are in this and that it has syrups or
+                                        extra sauces inside.</p>
+                                </Card.Content>
+                            </Card>
+                        </Card.Group>
+                    </GridColumn>
+                    <GridColumn>
+                        <Card.Group>
+                            <Card fluid>
+                                <Image floated="left" src={coffee} alt="Filler"/>
+                                <Card.Content>
+                                    <Card.Header>COFFEE DRINK EXAMPLE 2</Card.Header>
+                                    <p>Here is a description of the boba like what flavors are in this and that it has syrups or
+                                        extra sauces inside.</p>
+                                </Card.Content>
+                            </Card>
+                        </Card.Group>
+                    </GridColumn>
+                </GridRow>
 
-            <Card.Group>
-                <Card fluid>
-                    <Image floated="left" src={coffee} size='small' alt="Filler"/>
-                    <Card.Content>
-                        <Card.Header>COFFEE DRINK EXAMPLE 2</Card.Header>
-                        <p>Here is a description of the boba like what flavors are in this and that it has syrups or
-                            extra sauces inside.</p>
-                    </Card.Content>
-                </Card>
-            </Card.Group>
-
-            <Card.Group>
-                <Card fluid>
-                    <Image floated="left" src={milk} size='small' alt="Filler"/>
-                    <Card.Content>
-                        <Card.Header>SUBSTITUTES FOR MILK</Card.Header>
-                        <p>Here is a description of the boba like what flavors are in this and that it has syrups or
-                            extra sauces inside.</p>
-                    </Card.Content>
-                </Card>
-            </Card.Group>
+                <GridRow columns={2}>
+                    <GridColumn>
+                        <Card.Group>
+                            <Card fluid>
+                                <Image floated="left" src={filler} alt="Filler"/>
+                                <Card.Content>
+                                    <Card.Header>SUBSTITUTES FOR MILK</Card.Header>
+                                    <p>Here is a description of the boba like what flavors are in this and that it has syrups or
+                                        extra sauces inside.</p>
+                                </Card.Content>
+                            </Card>
+                        </Card.Group>
+                    </GridColumn>
+                    <GridColumn>
+                        <Card.Group>
+                            <Card fluid>
+                                <Image floated="left" src={coffee} alt="Filler"/>
+                                <Card.Content>
+                                    <Card.Header>COFFEE DRINK EXAMPLE 2</Card.Header>
+                                    <p>Here is a description of the boba like what flavors are in this and that it has syrups or
+                                        extra sauces inside.</p>
+                                </Card.Content>
+                            </Card>
+                        </Card.Group>
+                    </GridColumn>
+                </GridRow>
+            </Grid>
+            </div>
         </Container>
     )
 
