bc7dc19c0e287d4a9203e8351608cb8b750bf25d
Aubrie Usui <127988637+aubstah24@users.noreply.github.com>
2024-06-06 10:44:18 -1000

    Accidentally rebased previous changes.  fixed it back to the current UI that I had made.

--- my-app/src/Pages/AboutUsPage.js
+++ my-app/src/Pages/AboutUsPage.js
@@ -1,36 +1,130 @@
-import {Container, Divider, Form, FormButton, FormField, Header, TextArea} from "semantic-ui-react";
+import {Container, Divider, Grid, GridColumn, GridRow, Header, Image, Rail, Segment} from "semantic-ui-react";
 import React from "react";
+import family from '../images/fam-filler.jpeg';
+import kau from '../images/Kaucoffee.jpeg'
+import grnwell from '../images/greenwell.png';
+import ulu from '../images/uluwehi.png';
+import holu from '../images/Holualoa.jpg';
 
 export const AboutUsPage = () => {
     return (
         <Container className='menupage'>
 
             <div className="bio">
-                <Header as='h1' style={{padding: '10px', textAlign: 'center', fontFamily: 'Elephant'}}>OUR STORY</Header>
-                <p>We are a family-owned business and we wanted to provide affordable drinks while customers do their laundry.  We call it sip n' wash!
-                We take pride in our drink quality by going the extra lengths to ensure our ingredients are what we say they are.  </p>
+                <Header as='h1' style={{padding: '10px', textAlign: 'center', fontFamily: 'Elephant'}}>OUR
+                    STORY</Header>
+                <Image src={family} size='big' centered floated/>
+                <br/>
+                <p style={{fontFamily: 'Metro Nova Hawaiian Regular', fontSize: '14px', paddingLeft: "15px", paddingRight: "15px"}}>We are a family-owned business and we work hard to create a comfortable experience. Our boba shop is
+                    located in our King Laundromat area where you can do your clothes and enjoy a drink.
+                    We like call it sip n' wash! We take pride in our drink quality by going the extra lengths to ensure
+                    our ingredients are what we say they are.
+                    Supporting local is also in our best interest, which is why we personally gather coffee beans from
+                    Big Island farms.
+                </p>
             </div>
 
             <Divider/>
 
-            <Header as="h2" style={{padding: '10px', textAlign: 'center', fontFamily: 'Elephant'}}>Questions:</Header>
-            <p style={{textAlign: 'center'}}>If you have any questions or concerns, please fill out this form and we will get back to you as soon as we can.</p>
-            <Form style={{width: '70%', padding: '40px', left: '18%'}}>
-                <FormField>
-                    <label>First Name</label>
-                    <input placeholder="First Name"/>
-                </FormField>
-                <FormField>
-                    <label>Last Name</label>
-                    <input placeholder="First Name"/>
-                </FormField>
-                <FormField
-                    control={TextArea}
-                    label="Question/Issue:"
-                    placeholder="Describe your issue here or ask any questions..."/>
-                <FormButton>SUBMIT</FormButton>
-            </Form>
+            <Header as='h1' textAlign="center">Our Kope</Header>
+            <p style={{fontFamily: 'Metro Nova Hawaiian Regular', fontSize: '16px', textAlign: "center"}} >
+                    'Kope' is Hawaiian for 'Coffee' and we make sure our coffee is 100% locally grown.  Here are some of our local farms we use.
+                </p>
+
+            <Grid columns='equal' style={{padding: '40px'}}>
+                <GridRow>
+                    <GridColumn textAlign='center' width={5}>
+                        <Segment>Kaʻu Coffee Mill’s coffees are produced with 100% renewable energy from our
+                            Hydroelectric
+                            power system. This provides us with clean, sustainable energy while caring for our
+                            environment, for the planet and for the neighboring community.
+                        </Segment>
+                    </GridColumn>
+                    <GridColumn width={5}>
+                        <Segment>
+                            <Image src={kau} size='medium'/>
+                            <Rail position='right' attached>
+                                <Segment size='big'>Ka'u Coffee</Segment>
+                            </Rail>
+                        </Segment>
+                    </GridColumn>
+                </GridRow>
+
+                <GridRow>
+                    <GridColumn>
+
+                    </GridColumn>
+                    <GridColumn width={5}>
+                        <Segment>
+                            <Image src={ulu} size='medium'/>
+                            <Rail position='left' attached>
+                                <Segment size='big'>Uluwehi Coffee Farm</Segment>
+                            </Rail>
+                        </Segment>
+                    </GridColumn>
+                    <GridColumn textAlign='center' width={4}>
+                        <Segment>With exceptional quality potential at high altitudes, SL34 is known for flavors
+                            characterized by a complex citric acidity, heavy mouthfeel and a clean, sweet finish.
+                            SL34 is an Arabica coffee variety mostly found in Kenya.
+                        </Segment>
+                    </GridColumn>
+                </GridRow>
+
+                <GridRow columns={3}>
+                    <GridColumn textAlign='center' width={5}>
+                        <Segment>Award-winning coffee is grown in the heart of the Big Island and is handpicked at peak
+                            ripeness for rich flavor.
+                            Carefully selecting coffee beans to ensure that each batch is top quality.
+                            From bold to mild, classic to flavored, we have a roast for every coffee lover.
+                        </Segment>
+                    </GridColumn>
+                    <GridColumn width={5}>
+                        <Segment>
+                            <Image src={grnwell} size='medium'/>
+                            <Rail position='right' attached>
+                                <Segment size='big'>Greenwell Farms</Segment>
+                            </Rail>
+                        </Segment>
+                    </GridColumn>
+                </GridRow>
+
+                <GridRow columns={3}>
+                    <GridColumn width={6}>
+
+                    </GridColumn>
+                    <GridColumn width={6}>
+                        <Segment className='aboutcoffee'>
+                            <Image src={holu} size='large'/>
+                            <Rail position='left' attached>
+                                <Segment size='big'>Holualoa Kona Coffee</Segment>
+                            </Rail>
+                        </Segment>
+                    </GridColumn>
+                    <GridColumn textAlign='center' width={4}>
+                        <Segment>
+                            We’re Holualoa Kona Coffee Company, and we’re proud to be innovators of the highest
+                            standard. Our coffee and facility is certified Organic by Where Food Comes From.
+                            Through meticulous and reliable work, our company has become known as one of the top Organic
+                            Coffee Processors in Kona, Hawaii.
+                        </Segment>
+                    </GridColumn>
+                </GridRow>
+
+            </Grid>
+
+            <div className='aboutcoffee'>
+
+            </div>
+            <Divider/>
+
+            <Header as="h2" style={{padding: '10px', textAlign: 'center', fontFamily: 'Elephant', fontSize: '30px'}}>Contact
+                Us!</Header>
+            <p style={{textAlign: 'center'}}>If you have any questions or concerns, please email us with your name
+                at <b>Contact@DaMorningGrind.com</b>.</p>
+            <p style={{textAlign: 'center'}}>We will get back to you as soon as possible.</p>
+            <p style={{textAlign: 'center'}}>***If you are writing to us about an order, please include your order
+                number in the subject field.</p>
+
         </Container>
     )
-
 }
--- my-app/src/Pages/MenuCard.js
+++ my-app/src/Pages/MenuCard.js
@@ -1,11 +1,14 @@
+import {Header, Image} from "semantic-ui-react";
 import React, {useEffect, useState} from "react";
 import {supabase} from "../supabase_client";
-import {Card, CardDescription, CardMeta, Header, Image} from "semantic-ui-react";
-
+import {Row, Col} from "react-bootstrap";
+import beans from '../images/coffee-bean.png';
+import dairy from '../images/dairy-milk.png';
+import star from '../images/star.png';
 
 
 export const MenuCard = (props) => {
-    const {id, drink, price, description, caffeine, includesDairy, defaultAtr} = props.data;
+    const {id, drink, description, caffeine, includesDairy, category} = props.data;
     const [imageURL, setImageURL] = useState(null);
 
     useEffect(() => {
@@ -19,20 +22,26 @@ export const MenuCard = (props) => {
         setImageURL(publicURL.data.publicUrl)
     }
 
+
     return (
-        <div className="product">
-            <Card fluid>
-                <Header as='h2' textAlign='center' style={{paddingTop: "15px", fontFamily: 'Metro Nova Hawaiian Regular'}}>{drink}</Header>
-                <Image src={imageURL} size="large" centered={true}/>
-                <CardDescription style={{fontFamily: 'Metro Nova Hawaiian Regular'}}>{description}</CardDescription>
-                <p>{defaultAtr}</p>
-                <p>${price}</p>
-                <CardMeta>
-                    {caffeine ? "Contains Caffeine" : "Caffeine-Free"}
+        <>
+            <Row md={2} style={{padding:'20px', backgroundColor: 'white', justifyContent: 'space-between', borderRadius: '50px / 50px' }}>
+                <Col md={3}>
+                    <Image src={imageURL} size='medium' style={{borderRadius: '20%'}} />
+                </Col>
+                <Col md={9}>
+                    <Header as='h1' style={{fontFamily: 'Elephant'}}>{drink}</Header>
+                    <Header as='h3' style={{fontFamily: 'Metro Nova Hawaiian Light'}}>{description}</Header>
                     <br/>
-                    {includesDairy ? "Contains Dairy" : "Dairy-Free"}
-                </CardMeta>
-            </Card>
-        </div>
-    )
+                    <div style={{display: 'flex', flexDirection: 'row'}}>
+                        {caffeine ? <Image src={beans} size='mini'/> : null}
+                        {includesDairy ? <Image src={dairy} size='mini'/> : null}
+                        {category === 'Latte' ? <Image src={star} size='mini'/> : null}
+                    </div>
+                </Col>
+            </Row>
+            <br/>
+            <br/>
+        </>
+    );
 }
